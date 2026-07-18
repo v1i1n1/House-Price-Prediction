01664f56dec059d2c962c53ffa37501301ca1c8f
unknown <vinodhraj14081994@gmail.com>
2026-07-18 13:33:02 +0530

    Completed House Price Prediction Project

--- app.py
+++ app.py
@@ -50,5 +50,8 @@ def predict():
     prediction=round(prediction[0], 2)
 )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
